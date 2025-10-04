695c12796942b3d38ec6d91355fc72523876d1ac
raquel <raquelneves@MacBook-Air-de-Raquel.local>
2025-10-04 15:45:55 +0100

    Reset de tudo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,8 +17,11 @@ export default function Home() {
           <li>
             Get started by editing <code>src/app/page.tsx</code>.
           </li>
+          <li>oii</li>
           <li>Save and see your changes instantly.</li>
         </ol>
+
+        <h1>Sou a raquel</h1>
 
         <div className={styles.ctas}>
           <a
